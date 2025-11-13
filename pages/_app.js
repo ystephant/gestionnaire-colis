9be294855025404ac9f758c2f938f3d3833d1fe1
ystephant <134706137+ystephant@users.noreply.github.com>
2025-11-13 19:59:59 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    // Enregistrer le Service Worker
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker enregistré:', registration);
+        })
+        .catch((error) => {
+          console.error('Erreur Service Worker:', error);
+        });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
