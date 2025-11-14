14aebe10cbdcfffdcb181d7c9cd43e6dbaaafa2d
ystephant <134706137+ystephant@users.noreply.github.com>
2025-11-14 16:15:39 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="fr">
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#4f46e5" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </Head>
       <body>
         <Main />
