f1ac95923e63e0873369cfe63fe8ed7e25678b15
ystephant <134706137+ystephant@users.noreply.github.com>
2025-11-21 13:17:36 +0100

    Refactor app initialization to use OneSignal
    Removed service worker registration and added OneSignal initialization.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,13 @@
 import { useEffect } from 'react';
 import '../styles/globals.css';
+import { initOneSignal } from '../lib/onesignal';
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    // Enregistrer le Service Worker
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then((registration) => {
-          console.log('Service Worker enregistré:', registration);
-        })
-        .catch((error) => {
-          console.error('Erreur Service Worker:', error);
-        });
+    // Initialiser OneSignal
+    const username = localStorage.getItem('username');
+    if (username) {
+      initOneSignal(username);
     }
   }, []);
 
