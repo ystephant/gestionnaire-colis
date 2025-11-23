54455110ffa82acceb43ca60337be915fe67099a
ystephant <134706137+ystephant@users.noreply.github.com>
2025-11-23 15:08:40 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,15 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#4f46e5" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        {/* Charger le SDK OneSignal */}
+        <script 
+          src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" 
+          defer
+        />
       </Head>
       <body>
         <Main />
