743d69f68aa888289af561672493912b94c60e8f
ystephant <134706137+ystephant@users.noreply.github.com>
2025-12-07 19:15:19 +0100

    Change OneSignal REST API key in notify-colis-collected.js
    Updated OneSignal REST API key for notifications.

--- pages/api/notify-colis-collected.js
+++ pages/api/notify-colis-collected.js
@@ -70,4 +70,4 @@ export default async function handler(req, res) {
 Assurez-vous d'avoir sur Vercel :
 ```
 NEXT_PUBLIC_ONESIGNAL_APP_ID=24c0cb48-bcea-4953-934c-8d41632f3f16
-ONESIGNAL_REST_API_KEY=VOTRE_CLE_ICI
+ONESIGNAL_REST_API_KEY=itowpqwq4e5aviwnoxsh3frle
