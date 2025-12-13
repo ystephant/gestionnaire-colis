5e6e20cbfd31682659156ff2fcd21a3fa1470db6
ystephant <134706137+ystephant@users.noreply.github.com>
2025-12-13 17:55:52 +0100

    Update notify-colis-added.js

--- pages/api/notify-colis-added.js
+++ pages/api/notify-colis-added.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Basic ${process.env.ONESIGNAL_REST_API_KEY}`
+        'Authorization': `Bearer ${process.env.ONESIGNAL_REST_API_KEY}` // ✅ Bearer
       },
       body: JSON.stringify({
         app_id: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
