14e7ced4d369af51d93c1f7d21605a2aa9d3c300
ystephant <134706137+ystephant@users.noreply.github.com>
2026-02-14 10:46:58 +0100

    Update OneSignalSDKWorker.js

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,5 +1,1 @@
-// OneSignal Service Worker
-// Ce fichier DOIT être à la racine de votre site (dans le dossier public/)
-// Sans ce fichier, OneSignal ne peut PAS fonctionner !
-
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js');
