5a39e12822099c8ce30137a9a0919ce181e30d2e
ystephant <134706137+ystephant@users.noreply.github.com>
2026-02-14 10:05:59 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,36 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="fr">
       <Head>
+        {/* ✅ Manifest pour PWA et OneSignal */}
         <link rel="manifest" href="/manifest.json" />
+        
+        {/* ✅ Theme color pour navigateur mobile */}
+        <meta name="theme-color" content="#4f46e5" />
+        
+        {/* ✅ Apple touch icon */}
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        
+        {/* ✅ Favicon */}
+        <link rel="icon" href="/meeple_final.png" />
+        
+        {/* ✅ Meta pour iOS */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Le Petit Meeple" />
+        
+        {/* ✅ Meta pour Android */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        
+        {/* ✅ Meta description */}
+        <meta name="description" content="LePetitMeeple gère tout seul ses colis !" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
